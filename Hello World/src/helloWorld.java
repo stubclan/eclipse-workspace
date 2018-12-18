bbc8b84f49f4d90c68c0721e2c067f44c8760b4f
stubclan <aryal.sujan8@gmail.com>
2018-12-18 13:58:47 -0600

    Update helloWorld.java

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class helloWorld {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     String string1 = "Sujan";
     System.out.println("Hi, my name is " + string1);
     Scanner sc = new Scanner(System.in);
