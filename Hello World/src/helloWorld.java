e1d7390863746391d7bac22704eab0a851db984a
stubclan <aryal.sujan8@gmail.com>
2018-12-18 13:57:55 -0600

    fixed indentation

--- Hello World/src/helloWorld.java
+++ Hello World/src/helloWorld.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class helloWorld {
 
 	public static void main(String[] args) {
-	String string1 = "Sujan";
+    String string1 = "Sujan";
     System.out.println("Hi, my name is " + string1);
     Scanner sc = new Scanner(System.in);
     System.out.println("what's your name?");
     String input = sc.nextLine();
     System.out.println("Nice to meet you, " + input);
     sc.close();
-    }	
+  }	
 }
